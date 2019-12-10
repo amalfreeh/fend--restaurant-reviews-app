a39e011a7065b6223a074c57a6318cbd34929b05
Amal Almutairi <47970922+amalfreeh@users.noreply.github.com>
2019-12-11 00:49:49 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -161,7 +161,9 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = 'image of ' + restaurant.name + ' restaurant';
   li.append(image);
+
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
